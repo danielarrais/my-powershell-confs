fc0ae1a6f48ea6b78614c52923b35223d11c200a
danielarrais <danielarraiscarvalho@gmail.com>
2022-01-17 20:52:55 -0300

    Altera configuração de busca no autocomplete do powershell
    Possibilita ver opções que são compatíveis com o que foi digitado no terminal

--- Powershell/profile.ps1
+++ Powershell/profile.ps1
@@ -4,4 +4,7 @@ Import-Module PSReadLine
 
 Set-PoshPrompt -Theme material
 Set-PSReadLineOption -PredictionSource History
-Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -PredictionViewStyle InlineView # Default conf
+
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
